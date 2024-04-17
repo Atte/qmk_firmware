668db8cc56932eb89e85a6226643657a9710114d
Atte Virtanen <aten.email@gmail.com>
2024-04-17 21:20:58 +0300

    holdable ESC

--- keyboards/keychron/v4/ansi/keymaps/atte/keymap.c
+++ keyboards/keychron/v4/ansi/keymaps/atte/keymap.c
@@ -31,7 +31,7 @@ const uint16_t keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     [_FN1] = LAYOUT_ansi_61(
         QK_LEAD,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,
         KC_PSCR,  KC_F13,  KC_UP,   X(EURO), KC_P7,   KC_P8,   KC_P9,   KC_INS,  KC_HOME, KC_PGUP, XXXXXXX, KC_ARNG,  KC_MPRV, KC_MNXT,
-        _______,  KC_LEFT, KC_DOWN, KC_RGHT, KC_P4,   KC_P5,   KC_P6,   KC_DEL,  KC_END,  KC_PGDN, KC_OUML, KC_AUML,           KC_MPLY,
+        KC_ESC,   KC_LEFT, KC_DOWN, KC_RGHT, KC_P4,   KC_P5,   KC_P6,   KC_DEL,  KC_END,  KC_PGDN, KC_OUML, KC_AUML,           KC_MPLY,
         _______,  XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PDOT, XXXXXXX, XXXXXXX, XXXXXXX,                    _______,
         _______,  _______, _______,                   KC_P0,                                       _______, _______,  KC_RGUI, _______
     ),
